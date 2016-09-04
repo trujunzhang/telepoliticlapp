0658a29f6491d76b1a265c2e51c5121fe2413c20
trujunzhang <trujunzhang@gmail.com>
2016-09-04 21:47:08 +0800

    web app for politicl.

--- packages/nova-base-components/lib/posts/PostsLoadMore.jsx
+++ packages/nova-base-components/lib/posts/PostsLoadMore.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
+var Waypoint = require('react-waypoint');
 
 const PostsLoadMore = ({loadMore, count, totalCount}) => {
     return (
       <div>
+          <Waypoint
+            onEnter={({previousPosition, currentPosition, event}) => {
+                // do something useful!
+                loadMore(event);
+            }}
+            threshold={0}
+          />
           <a className="posts-load-more" onClick={loadMore}>Show More…</a>
       </div>
     )
