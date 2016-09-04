d886da0170ec6965deaf0228743916a2daa8f07f
trujunzhang <trujunzhang@gmail.com>
2016-09-04 19:20:53 +0800

    web app for politicl.

--- packages/nova-kadira/lib/server/kadira.js
+++
@@ -1,9 +0,0 @@
-import Telescope from 'meteor/nova:lib';
-
-Meteor.startup(function () {
-    var kadiraAppId = Telescope.settings.get('kadiraAppId');
-    var kadiraAppSecret = Telescope.settings.get('kadiraAppSecret');
-    if (process.env.NODE_ENV === "production" && !!kadiraAppId && !!kadiraAppSecret) {
-        Kadira.connect(kadiraAppId, kadiraAppSecret);
-    }
-});
