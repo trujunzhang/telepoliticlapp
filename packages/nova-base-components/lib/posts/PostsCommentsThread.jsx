76954dfcf34380dd6cb3488fb94e2da061cd556b
trujunzhang <trujunzhang@gmail.com>
2016-09-06 19:08:58 +0800

    web app for politicl.

--- packages/nova-base-components/lib/posts/PostsCommentsThread.jsx
+++ packages/nova-base-components/lib/posts/PostsCommentsThread.jsx
@@ -37,10 +37,10 @@ class PostsCommentsThread extends Component {
         const currentUser = this.props.currentUser;
 
         return (
-          <div class="discussion_cr2q_">
-              <h2 class="heading_2TFm8 heading_AsD8K title_2vHSk subtle_1BWOT base_3CbW2">Discussion</h2>
-              <a class="sortToggle_3nVmh text_3Wjo0 subtle_1BWOT base_3CbW2" href="#">most upvoted
-                  <span class="icon_2JDiK">
+          <div className="discussion_cr2q_">
+              <h2 className="heading_2TFm8 heading_AsD8K title_2vHSk subtle_1BWOT base_3CbW2">Discussion</h2>
+              <a className="sortToggle_3nVmh text_3Wjo0 subtle_1BWOT base_3CbW2" href="#">most upvoted
+                  <span className="icon_2JDiK">
                       <svg width="8" height="5" viewBox="0 0 8 5" xmlns="http://www.w3.org/2000/svg">
                         <path
                           d="M6.752 1.514C7.472.678 7.158 0 6.057 0H1.052C-.05 0-.332.654.426 1.46L2.38 3.54c.758.806 1.952.786 2.674-.054l1.698-1.972z"
