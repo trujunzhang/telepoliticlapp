f7e420e30648ca4b2129cfb25bbc83f805d9e20e
trujunzhang <trujunzhang@gmail.com>
2016-09-06 19:10:42 +0800

    web app for politicl.

--- packages/nova-base-components/lib/comments/CommentsNode.jsx
+++ packages/nova-base-components/lib/comments/CommentsNode.jsx
@@ -1,41 +1,42 @@
 import Telescope from 'meteor/nova:lib';
-import React, { PropTypes, Component } from 'react';
+import React, {PropTypes, Component} from 'react';
 
 class CommentsNode extends Component {
 
-  renderComment(comment) {
-    
-    return (
-      <Telescope.components.CommentsItem comment={comment} key={comment._id} currentUser={this.props.currentUser}/>
-    )
-  }
+    renderComment(comment) {
 
-  renderChildren(children) {
-    return (
-      <div className="comments-children">
-        {children.map(comment => <CommentsNode comment={comment} key={comment._id} currentUser={this.props.currentUser}/>)}
-      </div>
-    )
-  }
+        return (
+          <Telescope.components.CommentsItem comment={comment} key={comment._id} currentUser={this.props.currentUser}/>
+        )
+    }
 
-  render() {
+    renderChildren(children) {
+        return (
+          <div className="comments-children">
+              {children.map(comment => <CommentsNode comment={comment} key={comment._id}
+                                                     currentUser={this.props.currentUser}/>)}
+          </div>
+        )
+    }
 
-    const comment = this.props.comment;
-    const children = this.props.comment.childrenResults;
-    
-    return (
-      <div className="comments-node">
-        {this.renderComment(comment)}
-        {children ? this.renderChildren(children) : ""}
-      </div>
-    )
-  }
+    render() {
 
-};
+        const comment = this.props.comment;
+        const children = this.props.comment.childrenResults;
+
+        return (
+          <div className="comments-node">
+              {this.renderComment(comment)}
+              {children ? this.renderChildren(children) : ""}
+          </div>
+        )
+    }
+
+}
 
 CommentsNode.propTypes = {
-  comment: React.PropTypes.object.isRequired, // the current comment
-  currentUser: React.PropTypes.object, // the current user
-}
+    comment: React.PropTypes.object.isRequired, // the current comment
+    currentUser: React.PropTypes.object, // the current user
+};
 
 module.exports = CommentsNode;
